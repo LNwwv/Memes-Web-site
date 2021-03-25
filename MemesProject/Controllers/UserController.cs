fe11cc92dd199f8c54671262c7b2229287d2992c
Sebastian <sebatalar0@gmail.com>
2021-03-25 14:44:34 +0100

    Dodano aktywnosc uzytkownikow w panelu Twoje konto

--- MemesProject/Controllers/UserController.cs
+++ MemesProject/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Web.Mvc;
 using MemesProject.Models;
 using Microsoft.AspNet.Identity;
+using PagedList;
 
 namespace MemesProject.App_Start
 {
@@ -22,20 +23,45 @@ namespace MemesProject.App_Start
         // GET: User
         public ActionResult Index()
         {
+            var memesInDb = _context.MemeModels.ToList();
+            var commentsInDb = _context.Comments.ToList();
+
             var user = User.Identity.GetUserName();
+            var userId = User.Identity.GetUserId();
+            var memeId = _context.Comments.ToList().Where(m => m.UserId == userId);
+
+
+            ViewBag.SumOfMemes = _context.MemeModels.Count(m => m.CreatedBy == user);
 
             ViewBag.SumOfLikes = _context.MemeModels.ToList().Where(m => m.CreatedBy == user).Sum(m => m.Plus);
-            ViewBag.SumOfMinus = _context.MemeModels.ToList().Where(m => m.CreatedBy == user).Sum(m => m.Minus);
-            
+
+            ViewBag.SumOfComentary = _context.Comments.Count(m => m.UserId == user);
+
+
+
 
             return View();
         }
-        public ActionResult ShowMyMemes()
+        public ActionResult ShowMyMemes(string sortOrder, string currentFilter, string searchString, int? page)
         {
+            ViewBag.CurrentSort = sortOrder;
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
             var user = User.Identity.GetUserName();
             var memesInDb = _context.MemeModels.ToList().Where(m => m.CreatedBy == user);
 
-            return View(memesInDb);
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(memesInDb.ToPagedList(pageNumber, pageSize));
         }
         
 
